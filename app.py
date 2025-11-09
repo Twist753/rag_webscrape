6aa800d5c2dcd8b115465ef7a18bde9b55b116de
Twist <abhinavty753@gmail.com>
2025-11-10 01:19:35 +0530

    minor UI fix

--- app.py
+++ app.py
@@ -218,7 +218,6 @@ h1, h2, h3, label, .stMarkdown p {{
 st.markdown("""
 <div class="header">
     <div class="header-name">Abhinav Tyagi - abhinavty753@gmail.com</div>
-# ... (rest of your app is unchanged) ...
     <div class="header-links">
         <a href="https://www.linkedin.com/in/abhinav-tyagi-73373b281/" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
@@ -244,7 +243,8 @@ query = st.text_area(
 
 # 5. --- Button Layout ---
 # We create columns to place the buttons side-by-side
-col1, col2 = st.columns([4, 1]) # Main button gets 4/5 of the space
+# Ratios: 2 for main button, 1 for example button, 3 for empty spacer
+col1, col2, _ = st.columns([2, 1, 3]) 
 
 with col1:
     # 6. --- Main Button ---
